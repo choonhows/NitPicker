db4a5241df2f3e41a5a0e9d228f98781517b357d
Chrislyr Tan <cjtan2406@gmail.com>
2026-04-10 23:09:58 +0800

    Removed scroll button

--- src/pages/mockexamresults.tsx
+++ src/pages/mockexamresults.tsx
@@ -313,8 +313,12 @@ export default function MockExamResultsPage() {
                                                             <div className="border-l-4 border-black pl-3 max-h-72 overflow-y-auto
                                                                 [&::-webkit-scrollbar]:w-[3px]
                                                                 [&::-webkit-scrollbar-track]:bg-transparent
-                                                                [&::-webkit-scrollbar-thumb]:bg-black"
-                                                                 style={{scrollbarColor: "black transparent", scrollbarWidth: "auto", direction: "rtl"}}
+                                                                [&::-webkit-scrollbar-thumb]:bg-black
+                                                                [&::-webkit-scrollbar-button]:hidden"
+                                                                 style={{
+                                                                    scrollbarColor: "black transparent", 
+                                                                    scrollbarWidth: "auto", 
+                                                                    direction: "rtl"}}
                                                             >
                                                                 <div style={{direction: "ltr"}}>
                                                                     <p className="text-xs uppercase tracking-wide opacity-70 font-semibold">How to answer it</p>
